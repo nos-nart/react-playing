3c69db4222ad1527164c9a9d7b64dfee42ac8ffb
nos-nart <nosnart1510@gmail.com>
2019-12-17 00:55:15 +0700

    [+] uu

--- todoist/src/App.js
+++ todoist/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
-
+      hello world
     </div>
   );
 }
-
-export default App;
