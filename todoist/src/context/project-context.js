7589f042fcbc11d4cc757dadd2917129e1e9453d
nos-nart <nosnart1510@gmail.com>
2020-01-13 00:53:24 +0700

    [+] kk

---
+++ todoist/src/context/project-context.js
@@ -0,0 +1,12 @@
+import React, {createContext, useContext} from 'react';
+import {useProjects} from '../hooks';
+
+export const ProjectsContext = createContext({});
+
+export const ProjectsProvider = ({children}) => {
+    const { projects, setProjects } = useProjects();
+
+    return <ProjectsContext.Provider>
+        { children }
+    </ProjectsContext.Provider>
+}
