17a5632debe2cdcd1d9e89e2bfe1e8344c73797a
nos-nart <nosnart1510@gmail.com>
2020-02-10 00:51:40 +0700

    [-] yy

--- simple-todo-app/src/constants/ActionTypes.js
+++
@@ -1,5 +0,0 @@
-export const ADD = "ADD"
-export const LIST_ALL = "LIST_ALL"
-export const CHANGE_MESSAGE = "CHANGE_MESSAGE"
-export const DELETE = "DELETE"
-export const SEARCH = "SEARCH"
--- simple-todo-app/src/constants/Message.js
+++
@@ -1,3 +0,0 @@
-export const MSG_ADD = "An actions were added successful!"
-export const MSG_DELETE = "An action were removed!"
-export const MSG_WELCOME = "Welcome to my app <3!"
