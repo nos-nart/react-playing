717920ff50d3de12c7b5e1fa2c80ddbfad38ae41
son.tran <son.tran1@ntq-solution.com.vn>
2020-02-24 14:44:36 +0700

    [+] ss

---
+++ ex-google-map/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+
+function Map() {
+  return <GoogleMap 
+    defaultZoom={10}
+    defaultCenter={{lat: 21.027763, lng: 105.834160}}
+  />
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map))
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_GOOGLE_API_KEY}`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  );
+}
+
+export default App;
