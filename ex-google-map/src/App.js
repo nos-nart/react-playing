5eb1d3653fc45f0fda0939d40889c6b3502bbb05
son.tran <son.tran1@ntq-solution.com.vn>
2020-02-24 15:06:31 +0700

    [~] rr

--- ex-google-map/src/App.js
+++ ex-google-map/src/App.js
@@ -13,7 +13,7 @@ const WrappedMap = withScriptjs(withGoogleMap(Map))
 function App() {
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_GOOGLE_API_KEY}`}
+      <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_GOOGLE_API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
