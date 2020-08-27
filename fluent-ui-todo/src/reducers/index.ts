bd7f6929d2e2020f65dbc94304e66fc7acaddf7c
nos-nart <nosnart1510@gmail.com>
2020-08-28 00:26:33 +0700

    [+] jj

---
+++ fluent-ui-todo/src/reducers/index.ts
@@ -0,0 +1,43 @@
+import { IStore } from '../store';
+import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
+
+export const todosReducer = createReducer<IStore['todos']>(
+  {},
+  {
+    addTodo(state, action) {
+      state[action.id] = {
+        label: action.label,
+        completed: false,
+      }
+    },
+    remove(state, action) {
+      delete state[action.id]
+    },
+    clear(state, action) {
+      Object.keys(state).forEach(key => {
+        if (state[key].completed) {
+          delete state[key];
+        }
+      });
+    },
+    complete(state, action) {
+      state[action.id].completed = !state[action.id].completed;
+    },
+    edit(state, action) {
+      state[action.id].label = action.label;
+    }
+  }
+)
+
+export const filterReducer = createReducer<IStore['filter']>('all', {
+  setFilter(state, action) {
+    return action.filter;
+  }
+})
+
+export const reducer = combineReducers({
+  todos: todosReducer,
+  filter: filterReducer
+});
+
